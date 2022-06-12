b7380a4e29b6ed46c6fae8fffebd2f5e57779532
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-06-13 00:51:03 +0200

    My Class component of a title changer and a clock added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { useState } from 'react';
-import CounterContext from './Components/Contexts/CounterContexts';
-import Section from './Components/Section';
+import MyComponent from './Components/MyComponentClass';
 
 function App() {
-  const [count,setCount] = useState(0);
-
-  const change = ()=>(
-    setCount((prevState)=>(prevState+1))
-  )
   
-  console.log(change)
-
   return (
     <div>
-      <CounterContext.Provider value={{count:count,change:change}} >
-        <Section  />
-      </CounterContext.Provider>
+      <MyComponent />
     </div>
   );
 }
---
+++ src/Components/MyComponentClass.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export default class MyComponent extends React.Component{
+
+    state = {
+        count: 0,
+        date: new Date(),
+    };
+
+
+
+    componentDidMount(){
+        const { count } = this.state;
+        document.title = `Clicked ${count} times`;
+        this.Interval = setInterval(() =>{
+            this.tick()
+        },1000)
+    }
+
+    
+
+    UNSAFE_componentWillUpdate(){
+        const {count} = this.state;
+        document.title = `Clicked ${count} times`;
+    }
+
+
+    componentWillUnmount(){
+        clearInterval(this.Interval);
+    }
+    
+    buttonClick = () =>{  
+        return(
+            this.setState(
+                (prevState)=>(
+                    {count :prevState.count + 1}
+                )
+            )
+        )
+    }
+
+    tick = ()=>{ 
+        this.setState({
+            date: new Date(),
+        })
+    }
+
+
+    render() {
+        const {count,date} = this.state;
+        return (
+            <div>
+                <p>
+                    {count}
+                </p>
+                <button type="button" onClick={this.buttonClick}>
+                    Click to change title
+                </button>
+                <p>
+                   Current Time :  {date.toLocaleTimeString()}
+                </p>
+            </div>
+        )
+    }
+}
