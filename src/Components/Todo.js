7784fa95185a1b7b04b93100df529b41f1fea79b
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-06-12 19:12:36 +0200

    How to change the whole object value or the array value using speared operator in useState(function) during calling the setState function

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -4,28 +4,40 @@ export default function Todo() {
     // ** Alaways use use state at the top of the function 
     // ** Never use this useState() state function in the other function of your own function or class
     // ** useState() takes 2 params as a function and return an array of 2 elements [defaultValue,setValue()]
-    
-    const [todo, setTodo] = useState('');
-    const [warning, setWarning] = useState(null);
 
-    const handleTodoChange = (e)=>{
-        const inputTodo = e.target.value;
-        const warningTodo = inputTodo.includes('.js')?"Warning: Learn js":null;
-        
-        setTodo(e.target.value);
-        setWarning(warningTodo);
-    };
+    const [todo, setTodo] = useState({
+        title: '',
+        description: '',
+    });
+  
+    const {title, description} = todo;
+
+    const titleChange = (e) =>{
+        setTodo({
+            ...todo,
+            title: e.target.value,
+        })
+    }
+
+
+    const descriptionChange = (e) =>{
+        setTodo({
+            ...todo,
+            description: e.target.value,
+        })
+    }
 
     return (
         <div>
-            <p>{todo}</p>
             <p>
-               <textarea type="text" value={todo} onChange={handleTodoChange} placeholder="Enter Todo" />
+                {title}
+                {description}
             </p>
-            <hr />
-            <h2>
-                {warning === null?'Good Choice':'Warning: JS LEarn'}
-            </h2>
+            <p>
+                <textarea type="text" value={title} onChange={titleChange} />
+
+                <textarea type="text" value={description} onChange={descriptionChange} />                
+            </p>
         </div>
     )
 
